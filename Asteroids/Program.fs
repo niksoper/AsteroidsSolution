f4a1001a5204a20ae09af7c38bfa46ffa0e83da0
Nick Soper <nsoper@scottlogic.com>
2015-06-05 14:03:23 +0100

    Print 1 on keyDown or mouseDown

--- Asteroids/Program.fs
+++ Asteroids/Program.fs
@@ -4,118 +4,22 @@ open System
 
 open OpenTK
 open OpenTK.Graphics
-open OpenTK.Graphics.OpenGL
-open OpenTK.Input
-
-open Domain
-open Geometry
-open Render
-
-(*
-    Note: While we are calling this an Asteroids clone, we may deviate from it...
-
-    References: 
-    http://www.opentk.com/
-    Reactive Programming intro with observables and f# http://fsharpforfunandprofit.com/posts/concurrency-reactive/ 
-
-    Tasks:
-        - Refactor the open GL Program.fs 
-        - Reach functionality goal of : 
-            Deal with basic ship movement: 
-                1. Left and Right arrow should rotate the ship
-                2. Forward arrow should cause acceleration
-                3. Backwards arrow should cause deceleration
-                4. Asteroids style position wrap around when an object leaves the screen. PLay the asteroids game if you cant remember it!
-        - Test as much as possible!
-*)
 
 [<EntryPoint>]
 let main _ = 
     use game = new GameWindow(800, 600, GraphicsMode.Default, "Asteroids")
+            
+    let print x = printfn "%A" x
+    let mapSingle = (fun _ -> 1)
 
-    let updateFrame (state :GameState) =
-        match state.Running with 
-        | Continue -> ()
-        | Stop -> game.Exit()
+    let printSingleWhen event =
+        event
+        |> Observable.map mapSingle
+        |> Observable.subscribe print
 
-    let renderFrame (state: GameState)  =
+    use mouseDownSub    = printSingleWhen game.MouseDown
+    use keyDownSub      = printSingleWhen game.KeyDown
 
-        //OpenGL Stuff to set view
-        GL.Clear(ClearBufferMask.ColorBufferBit ||| ClearBufferMask.DepthBufferBit)
-        let mutable modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY)
-        GL.MatrixMode(MatrixMode.Modelview)
-        GL.LoadMatrix(&modelview)
-
-        state.Ship |> draw ship
-
-        // Game is double buffered
-        game.SwapBuffers()
-
-    //Handle keydownEvents and transform them into state changes 
-    //Hint (To get the best behaviour, you may need to deal with key up, etc events)
-    let keyDown (args: KeyboardKeyEventArgs) =
-        match args.Key with
-        | Key.Escape ->  UserStateChange.EndGame
-        | Key.Up -> SpeedUp
-        | Key.Down -> SlowDown
-        | Key.Right -> UserStateChange.Rotate Domain.Settings.RotateSpeed
-        | Key.Left -> UserStateChange.Rotate -Domain.Settings.RotateSpeed
-        | _ -> UserStateChange.NoChange
-
-    let updateGameState (state: GameState)  change = 
-        let newState = 
-            match change with 
-            | ChangePosition posChange  -> {state with Ship = state.Ship |> ShipChange.move}
-            | Rotate rotation           -> {state with Ship = state.Ship |> ShipChange.rotate rotation}
-            | SpeedUp                   -> {state with Ship = state.Ship |> ShipChange.thrust}
-            | SlowDown                  -> {state with Ship = state.Ship |> ShipChange.reverse}
-            | EndGame                   -> {state with Running=Stop}
-            | NoChange                  -> state
-        //printfn "%A" newState
-        newState
-
-    let tick (state: GameState) (e: FrameEventArgs) = 
-        let s = state.Ship
-        let newShip = 
-            match s.Thrust with
-            | None -> 
-                //printfn "Not moving!"
-                s
-            | Forward -> 
-                let newPosition = {X = s.Position.X; Y = s.Position.Y + 0.01}
-                //printfn "%A" newPosition
-                {state.Ship with Position = newPosition}
-        let newTime = state.Time + e.Time
-        {state with Time = newTime; Ship = newShip}
-
-    use loadSubscription = game.Load.Subscribe (WindowHandlers.load game)
-    use resizeSubscription = game.Resize.Subscribe (WindowHandlers.resize game)   
-
-    (*Below, the game state is being stored in a reference cell instead of being passed through the observable functions
-        The reason is that my original approach that passed the state on directly caused a memory leak. 
-        I'm not sure if it was due to my original code, or an issue with the library, but this a simple alternative that means the code is nearly the same,
-        with a little local mutability.
-        
-        Also, reference cell is used instead of a mutable value because mutable values cannot be captured by lambdas. 
-        For a longer explaination see: https://lorgonblog.wordpress.com/2008/11/12/on-lambdas-capture-and-mutability/ 
-        Also msdn reference : https://msdn.microsoft.com/en-us/library/dd233186.aspx*)
-    let currentGameState = ref Domain.initialState
-
-    use updateGameStateSub = 
-        game.KeyDown
-        |> Observable.map keyDown
-        |> Observable.scan updateGameState Domain.initialState 
-        |> Observable.subscribe (fun state -> currentGameState := state)
-
-    use renderFrameSub = 
-        game.RenderFrame
-        |> Observable.subscribe(fun _ -> renderFrame !currentGameState)
-
-    use updateFrameSub = 
-        game.UpdateFrame
-        |> Observable.scan tick !currentGameState 
-        |> Observable.subscribe(fun _ -> updateFrame !currentGameState)
-        
     game.Run(60.0)
 
     0 
