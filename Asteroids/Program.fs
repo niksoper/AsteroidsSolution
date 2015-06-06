306c7ed12ebebb99ec7bbc320439738e52b2472f
Nick Soper <nsoper@scottlogic.com>
2015-06-06 17:57:55 +0100

    Add vs Subscribe: Disposing a subscribed stream

--- Asteroids/Program.fs
+++ Asteroids/Program.fs
@@ -4,21 +4,28 @@ open System
 
 open OpenTK
 open OpenTK.Graphics
+open OpenTK.Input
 
 [<EntryPoint>]
 let main _ = 
     use game = new GameWindow(800, 600, GraphicsMode.Default, "Asteroids")
             
     let print x = printfn "%A" x
-    let mapSingle = (fun _ -> 1)
+    let mapSingle x = 1
 
     let printSingleWhen event =
         event
         |> Observable.map mapSingle
         |> Observable.subscribe print
 
-    use mouseDownSub    = printSingleWhen game.MouseDown
-    use keyDownSub      = printSingleWhen game.KeyDown
+    use mouseSub = printSingleWhen game.MouseDown
+    use keySub = printSingleWhen game.KeyDown
+
+    game.KeyDown
+    |> Observable.filter (fun args -> args.Key = Key.Escape)
+    |> Observable.add (fun _ -> 
+        print "Disposing mouse down subscription"
+        mouseSub.Dispose())
 
     game.Run(60.0)
 
