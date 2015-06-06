0173e6487238d40960f25c9317cce009e39168ee
Nick Soper <nsoper@scottlogic.com>
2015-06-06 19:15:19 +0100

    Split: Even strings, odd numbers

--- Asteroids/Program.fs
+++ Asteroids/Program.fs
@@ -12,20 +12,23 @@ let main _ =
 
     use game = new GameWindow(800, 600, GraphicsMode.Default, "Observable")
     
+    let print x = printfn "%A" x
+
     let mapRandom x = 
         let rnd = new Random();
         rnd.Next(10)
 
-    let checkEven x = 
-        x % 2 = 0
+    let checkEven = function
+        | x when x % 2 = 0 -> Choice1Of2 <| sprintf "%d!!! We love even numbers :=D" x
+        | x -> Choice2Of2 x
 
     let evens, odds = 
         game.MouseDown
         |> Observable.map mapRandom
-        |> Observable.partition checkEven
+        |> Observable.split checkEven
 
-    evens |> Observable.add (fun even -> printfn "%d!!! We love even numbers :=D" even)
-    odds |> Observable.add (fun odd -> printfn "%d. How sad, an odd number :-(" odd)
+    evens |> Observable.add print
+    odds |> Observable.add print
     
     game.Run(60.0)
 
