cee0e89a0517b6ca9126f6a2a9e03064f38282e1
Nick Soper <nsoper@scottlogic.com>
2015-06-08 23:58:45 +0100

    Very dodgy acceleration

--- Asteroids/ShipChange.fs
+++ Asteroids/ShipChange.fs
@@ -4,15 +4,31 @@ open Geometry
 
 open Domain
 
+let private newVelocity thrust direction (u:Vector) =
+    let x,y = rotate thrust u.Dx u.Dy direction
+    {Dx = x; Dy = y}
+    
 let accelerate a ship = 
     {ship with Thrust = Some(a)}
-    
+
 let updateSpin newSpin ship = {ship with Spin = newSpin}
 
 let move t ship =
+    let p = ship.Position
+    let v = ship.Velocity
+    
     let newOrientation = ship.Orientation + ship.Spin
-    let newPos = 
+    let velocityChange = 
         match ship.Thrust with
-        | Some a    -> rotatePoint a ship.Orientation ship.Position
-        | None      -> ship.Position
-    {ship with Orientation = newOrientation; Position = newPos}  
+        | Some a    -> newVelocity a newOrientation v
+        | None      -> v
+
+    print velocityChange
+
+    let newV = {
+        Dx = v.Dx + velocityChange.Dx
+        Dy = v.Dy + velocityChange.Dy
+    }
+
+    let newPos = {X = p.X + newV.Dx; Y = p.Y + newV.Dy}
+    {ship with Orientation = newOrientation; Position = newPos; Velocity = newV}  
